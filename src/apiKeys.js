a0ef1b4ecdcce0c58f77cf6b7d4f0c058a5ecc89
ajaygangwar945 <ajaygangwar945@gmail.com>
2026-01-26 16:09:58 +0530

    fix: resolve eslint error in apiKeys.js for netlify build

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,6 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
-export default {
+const apiKeys = {
   key: "474bb742be91a4a858fec425ddd3f1a0",
   base: "https://api.openweathermap.org/data/2.5/",
 };
+export default apiKeys;
